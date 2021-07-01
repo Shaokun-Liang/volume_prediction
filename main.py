0e5e6fce44464ed7d87172047e5b8eea5a064789
Whitehole-mark <201821090110@stu.zuel.edu.cn>
2021-07-01 19:11:14 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from util.util import run
+
+
+run()
+
+
+
---
+++ util/util.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def cal(x: int):
+    return x + 1
+
+
+def run():
+    print(cal(9))
